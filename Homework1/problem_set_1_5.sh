d920f39494ddfc38606d93013f44370e720cf9a9
JM-13 <visockas13@gmail.^com>
2024-03-09 20:02:30 +0200

    added argument check and started deep functionallity

--- Homework1/problem_set_1_5.sh
+++ Homework1/problem_set_1_5.sh
@@ -7,15 +7,49 @@ c_red=$(tput setaf 1)    # \033[31
 tmp_f1='/tmp/folder1.txt'
 tmp_f2='/tmp/folder2.txt'
 
-tree -U --noreport $1 > $tmp_f1
-tree -U --noreport $2 > $tmp_f2
+deep=false
 
+case $# in
+2) folder1=$1; folder2=$2 ;;
+3) case $1 in
+    -deep) folder1=$2; folder2=$3; deep=true; ;;
+    *) exit 1 ;;
+   esac
+;;
 
-sed -i 's/└──/├──/g' $tmp_f1 $tmp_f2
-sed  -i "s/\s\{4,\}/│   /g" $tmp_f1 $tmp_f2
+*) exit 1 ;;
+esac
+
+tree -a --noreport --filesfirst $folder1 > $tmp_f1
+tree -a --noreport --filesfirst $folder2 > $tmp_f2
+
+if $deep; then
+    tmp_fd1='/tmp/folderd1.txt'
+    tmp_fd2='/tmp/folderd2.txt'
+
+    tree -af --noreport --filesfirst $folder1 > $tmp_fd1
+    tree -af --noreport --filesfirst $folder2 > $tmp_fd2
+
+    sed -i 's/└──/├──/g'       $tmp_fd1 $tmp_fd2
+    sed -i 's/│  /│     /g'    $tmp_fd1 $tmp_fd2
+    sed -i "s/\s\{4,\}/│   /g" $tmp_fd1 $tmp_fd2
+
+    sed -i 's/├── //g'   $tmp_fd1 $tmp_fd2
+    sed -i 's/│     //g' $tmp_fd1 $tmp_fd2
+    sed -i 's/│   //g'   $tmp_fd1 $tmp_fd2
+    sed -i 's/│  //g'    $tmp_fd1 $tmp_fd2
+fi
+
+sed -i 's/└──/├──/g'       $tmp_f1 $tmp_f2
+sed -i 's/│  /│     /g'    $tmp_f1 $tmp_f2
+sed -i "s/\s\{4,\}/│   /g" $tmp_f1 $tmp_f2
 
 matching1=$(grep -xnf $tmp_f2 $tmp_f1 | cut -f1 -d:)
 matching2=$(grep -xnf $tmp_f1 $tmp_f2 | cut -f1 -d:)
+
+if $deep; then
+:
+fi
 
 sed -i "s/^/$c_red/" $tmp_f1 $tmp_f2
 
