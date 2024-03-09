1340ebef29cb4befc7deb54befb4f8ea3a30896c
JM-13 <visockas13@gmail.^com>
2024-03-09 20:13:26 +0200

    fixed output error when line amount exceeded

--- Homework1/problem_set_1_5.sh
+++ Homework1/problem_set_1_5.sh
@@ -10,9 +10,9 @@ tmp_f2='/tmp/folder2.txt'
 deep=false
 
 case $# in
-2) folder1=$1; folder2=$2 ;;
+2) folder1="$1"; folder2="$2" ;;
 3) case $1 in
-    -deep) folder1=$2; folder2=$3; deep=true; ;;
+    -deep) folder1="$2"; folder2="$3"; deep=true; ;;
     *) exit 1 ;;
    esac
 ;;
@@ -20,15 +20,15 @@ case $# in
 *) exit 1 ;;
 esac
 
-tree -a --noreport --filesfirst $folder1 > $tmp_f1
-tree -a --noreport --filesfirst $folder2 > $tmp_f2
+tree -a --noreport --filesfirst "$folder1" > $tmp_f1
+tree -a --noreport --filesfirst "$folder2" > $tmp_f2
 
 if $deep; then
     tmp_fd1='/tmp/folderd1.txt'
     tmp_fd2='/tmp/folderd2.txt'
 
-    tree -af --noreport --filesfirst $folder1 > $tmp_fd1
-    tree -af --noreport --filesfirst $folder2 > $tmp_fd2
+    tree -af --noreport --filesfirst "$folder1" > $tmp_fd1
+    tree -af --noreport --filesfirst "$folder2" > $tmp_fd2
 
     sed -i 's/└──/├──/g'       $tmp_fd1 $tmp_fd2
     sed -i 's/│  /│     /g'    $tmp_fd1 $tmp_fd2
@@ -79,7 +79,14 @@ pad=$(printf '%*s' "$padding")
 for i in $(seq $len)
 do
     line1=$(head -n $i $tmp_f1 | tail -1)
+    if [[ $i -gt $len1 ]]; then
+        line1=" "
+    fi
+
     line2=$(head -n $i $tmp_f2 | tail -1)
+    if [[ $i -gt $len2 ]]; then
+        line2=" "
+    fi
 
     printf "%s %s %s\n" "$line1" "${pad:${#line1}}" "$line2"
 done
