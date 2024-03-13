24ed697696d64b5c9213ae12c49ec865d0cd1fef
JM-13 <visockas13@gmail.^com>
2024-03-13 21:36:01 +0200

    made deep comparison work and fixed some other issues

--- Homework1/problem_set_1_5.sh
+++ Homework1/problem_set_1_5.sh
@@ -30,42 +30,72 @@ if $deep; then
     tree -af --noreport --filesfirst "$folder1" > $tmp_fd1
     tree -af --noreport --filesfirst "$folder2" > $tmp_fd2
 
-    sed -i 's/└──/├──/g'       $tmp_fd1 $tmp_fd2
-    sed -i 's/│  /│     /g'    $tmp_fd1 $tmp_fd2
-    sed -i "s/\s\{4,\}/│   /g" $tmp_fd1 $tmp_fd2
+    sed -i 's/└──/├──/g'      $tmp_fd1 $tmp_fd2
+    sed -i 's/│   /│  /g'     $tmp_fd1 $tmp_fd2
+    sed -i "s/\s\{4,\}/│  /g" $tmp_fd1 $tmp_fd2
 
     sed -i 's/├── //g'   $tmp_fd1 $tmp_fd2
-    sed -i 's/│     //g' $tmp_fd1 $tmp_fd2
-    sed -i 's/│   //g'   $tmp_fd1 $tmp_fd2
     sed -i 's/│  //g'    $tmp_fd1 $tmp_fd2
 fi
 
 sed -i 's/└──/├──/g'       $tmp_f1 $tmp_f2
-sed -i 's/│  /│     /g'    $tmp_f1 $tmp_f2
-sed -i "s/\s\{4,\}/│   /g" $tmp_f1 $tmp_f2
+sed -i 's/│   /│  /g'    $tmp_f1 $tmp_f2
+sed -i "s/\s\{4,\}/│  /g" $tmp_f1 $tmp_f2
 
-matching1=$(grep -xnf $tmp_f2 $tmp_f1 | cut -f1 -d:)
-matching2=$(grep -xnf $tmp_f1 $tmp_f2 | cut -f1 -d:)
 
-if $deep; then
-:
+matching1=($(grep -xnf $tmp_f2 $tmp_f1 | cut -f1 -d:))
+matching2=($(grep -xnf $tmp_f1 $tmp_f2 | cut -f1 -d:))
+
+if $deep && [[ ${#matching1[@]} -gt 0 ]]; then
+    imposters_in_1=()
+    imposters_in_2=()
+    tmp_tmp_f1='/tmp/tmp_sha1.txt'
+    tmp_tmp_f2='/tmp/tmp_sha2.txt'
+
+    for i in $(seq ${#matching1[@]}); do
+        num=$(($i-1))
+
+        mach_1=$(head -n ${matching1[$num]} $tmp_fd1 | tail -1)
+        mach_2=$(head -n ${matching2[$num]} $tmp_fd2 | tail -1)
+        if [[ -d $mach_1 ]]; then
+            :
+        else
+            sha256sum "$mach_1" | cut -d ' ' -f 1 > "$tmp_tmp_f1"
+            sha256sum "$mach_2" | cut -d ' ' -f 1 > "$tmp_tmp_f2"
+            sha_mach=$(grep -xnf "$tmp_tmp_f1" "$tmp_tmp_f2" | cut -f1 -d:)
+
+            if [[ $sha_mach -gt 0 ]]; then
+                :
+            else
+                imposters_in_1+=(${matching1[$num]})
+                imposters_in_2+=(${matching2[$num]})
+            fi
+        fi
+    done
 fi
 
 sed -i "s/^/$c_red/" $tmp_f1 $tmp_f2
 
-for i in $matching1
-do
+for i in ${matching1[@]}; do
     sed -i "$i s!\[31!\[96!" $tmp_f1
 done
 
-for i in $matching2
-do
+for i in ${matching2[@]}; do
     sed -i "$i s!\[31!\[96!" $tmp_f2
 done
 
+if $deep; then
+    for i in ${imposters_in_1[@]}; do
+        sed -i "$i s!\[96!\[33!" $tmp_f1
+    done
 
-len1=$(wc -l <  $tmp_f1)
-len2=$(wc -l <  $tmp_f2)
+    for i in ${imposters_in_2[@]}; do
+        sed -i "$i s!\[96!\[33!" $tmp_f2
+    done
+fi
+
+len1=$(wc -l < $tmp_f1)
+len2=$(wc -l < $tmp_f2)
 
 if [[ $len1 -gt $len2 ]]; then
     len=$len1
@@ -76,19 +106,18 @@ fi
 padding=$(($(wc -L < $tmp_f1)+1))
 pad=$(printf '%*s' "$padding")
 
-for i in $(seq $len)
-do
+for i in $(seq $len); do
     line1=$(head -n $i $tmp_f1 | tail -1)
     if [[ $i -gt $len1 ]]; then
-        line1=" "
+        line1="$c_red"
     fi
 
     line2=$(head -n $i $tmp_f2 | tail -1)
     if [[ $i -gt $len2 ]]; then
-        line2=" "
+        line2="$c_red"
     fi
 
-    printf "%s %s %s\n" "$line1" "${pad:${#line1}}" "$line2"
+    printf "%s%s%s\n" "$line1" "${pad:${#line1}}" "$line2"
 done
 
 tput sgr0; echo ""; exit 0;
